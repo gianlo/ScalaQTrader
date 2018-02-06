cd2db45821ba76012f7234b0d4ab03105ed625d1
gianlo <gianlo@gmail.com>
2018-02-06 15:24:45 +0000

    plain q-learning algo

--- src/main/scala/net/gianlorenzo/scalaqtrader/learning/QLearnAlgo.scala
+++ src/main/scala/net/gianlorenzo/scalaqtrader/learning/QLearnAlgo.scala
@@ -2,28 +2,77 @@ package net.gianlorenzo.scalaqtrader.learning
 
 import net.gianlorenzo.scalaqtrader.model.{Fraction, Q}
 
-class QLearnAlgo[S, A](learningRate: Fraction, discountFactor: Fraction) {
+
+trait Randomizer[+T]{
+  def sample(): T
+}
+
+trait Enumerate[+T]{
+  def getAll(): Seq[T]
+}
+
+trait ChooseDefault[+T]{
+  def default: T
+}
+
+class QLearnAlgo[S, A](learningRate: Fraction, discountFactor: Fraction, randomActionFraction: Fraction) {
 
   import Fraction._
 
-  def initQ: Q[S,A] = Q.empty[S, A]
+  private val actionChooserRandomiser = new scala.util.Random()
 
-  def train[I](trainingData: Stream[(I, S, Double)]): Q[S, A] = {
+  private def chooseNextActionDeterministically: Boolean = actionChooserRandomiser.nextDouble() > 1.0 - randomActionFraction
 
-    implicit def dataToExp(d: (I, S, Double)): (S, Double) = (d._2, d._3)
+  def initQ(implicit es: Enumerate[S], da: ChooseDefault[A]): Q[S, A] = es.getAll().foldLeft(Q.empty[S, A]){case (q, s) => q.update(s, da.default, 0.0)}
 
-    def go(experience: (S, Double), data: Stream[(I, S, Double)], qcurr: Q[S,A]): Q[S, A] = data match {
-      case (_, nextState, nextReward) #:: tail => {
+  def train(trainingData: Seq[(S, Double)])(implicit ar: Randomizer[A], es: Enumerate[S], da: ChooseDefault[A]): Q[S, A] = {
+
+    def go(experience: (S, Double), data: Seq[(S, Double)], qcurr: Q[S, A])(implicit ar: Randomizer[A], es: Enumerate[S], da: ChooseDefault[A]): Q[S, A] = data match {
+      case (nextState, nextReward) +: tail => {
         val (state, reward) = experience
-        val action = qcurr.m(state).maxBy(_._2)._1
-        val learned = reward + (discountFactor * qcurr.m(state)(action))
-        val qupdate = (1.0 - learningRate) * qcurr.m(state)(action) + learningRate * learned
-        go((nextState, nextReward), tail, qcurr.update(state, action, qupdate))
+        val action = if (chooseNextActionDeterministically) {
+          qcurr.m(state).maxBy(_._2)._1
+        } else {
+          ar.sample()
+        }
+        val qUpdate = for {
+          value <-  qcurr.m(state).get(action)
+          learned = reward + (discountFactor * value)
+        } yield (1.0 - learningRate) * value + learningRate * learned
+        go((nextState, nextReward), tail, qcurr.update(state, action, qUpdate.getOrElse(0.0)))
       }
       case _ => qcurr
     }
 
     go(trainingData.head, trainingData.tail, initQ)
   }
+}
 
+
+object QLearnAlgo {
+  def main(args: Array[String]): Unit = {
+    val qlearner = new QLearnAlgo[Int, Int](Fraction(0.8), Fraction(0.2), Fraction(0.01))
+
+    implicit val intR = new Randomizer[Int] {
+      private val rnd = new scala.util.Random()
+      override def sample(): Int = 2-rnd.nextInt(5)
+    }
+
+    implicit val intD = new ChooseDefault[Int] {
+      override def default: Int = 0
+    }
+
+    implicit val intE = new Enumerate[Int] {
+      override def getAll(): Seq[Int] = (1 to 10)
+    }
+
+    val trainingSet = (1 to 100).map(n => ((n % 10) + 1, n.toDouble + 0.03))
+    val q = qlearner.train(trainingSet)
+
+    val policy = q.m.mapValues(av => av.maxBy(_._2)._1)
+    println("Q")
+    println(q.m)
+    println("Policy")
+    println(policy)
+  }
 }
